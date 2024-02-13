90da7984271a553f693c74174bdb100de0b525b9
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2024-02-13 21:39:07 +0000

    add completion events

--- internal/feedback/event/event.go
+++ internal/feedback/event/event.go
@@ -2,24 +2,50 @@ package event
 
 import "time"
 
-type BaseModel struct {
-	id          string
+type base struct {
+	id          string // the message ID might not be unique
 	timestamp   time.Time
 	challengeID string // is this needed?
 }
 
-func (bm BaseModel) ID() string {
-	return bm.id
+func NewEvent(id string) base {
+	return base{
+		id:        id,
+		timestamp: time.Now().UTC(),
+	}
 }
 
-func (bm BaseModel) Timestamp() time.Time {
-	return bm.timestamp
+func (b base) ID() string {
+	return b.id
 }
 
-func (bm BaseModel) ChallengeID() string {
-	return bm.challengeID
+func (b base) Timestamp() time.Time {
+	return b.timestamp
 }
 
-type CompletionEvent struct {
-	BaseModel
+func (b base) ChallengeID() string {
+	return b.challengeID
+}
+
+type completion struct {
+	base
+	userID   string
+	duration time.Duration
+}
+
+// Completion holds the details of a user-reported challenge completion
+func (b base) Completion(userID string, duration time.Duration) completion {
+	return completion{
+		base:     b,
+		userID:   userID,
+		duration: duration,
+	}
+}
+
+func (c completion) UserID() string {
+	return c.userID
+}
+
+func (c completion) Duration() time.Duration {
+	return c.duration
 }
